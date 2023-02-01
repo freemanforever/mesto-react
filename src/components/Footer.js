e6c24a9a711a81d4ccd4fb0df16176dbe97f2112
DmitriyL <dima-leg@yandex.ru>
2023-02-02 00:04:41 +0300

    fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,4 @@
+import React from "react";
 export default Footer;
 
 function Footer() {
--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,3 +1,4 @@
+import React from "react";
 import closeButton from "../images/closeButton.svg";
 
 function ImagePopup(props) {
